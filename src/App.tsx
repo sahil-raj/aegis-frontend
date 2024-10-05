70ce6a51ee1a6fcaacdc9afab32e3e5315c8d1cd
sahil-raj <sahilrajaaru@gmail.com>
2024-10-06 01:27:14 +0530

    chore: initilization shadcn, aceternity

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { AuroraBackground } from "./components/ui/aurora-background";
+
+const App = () => {
+  return (
+    <div>
+      <AuroraBackground></AuroraBackground>
+    </div>
+  );
+};
+
+export default App;
